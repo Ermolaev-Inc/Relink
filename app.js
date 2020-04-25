c0fe02ab3f2639802114018aef5371251265e434
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-04-25 10:32:40 +0500

    Connect to the MongoDB

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const express = require('express');
+const express = require("express");
 const config = require("config");
+const mongoose = require("mongoose"); 
 
 const app = express();
 
-const PORT =  config.get('port') || 5000;
+const PORT =  config.get("port") || 5000;
 
-app.listen(PORT, () => {console.log(`Start on ${{PORT}} port...`)});
+async function start() {
+    try {
+        await mongoose.connect(config.get("mongoUri"), {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        app.listen(PORT, () => {console.log(`Start on ${{PORT}} port...`)});
+    } catch (error) {
+        console.log("Server error", error.message);
+        process.exit(1);
+    };
+};
+
+start();
