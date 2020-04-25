92d66c1e38a69f90736c58aba9834b89eb6f353e
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-04-25 18:03:12 +0500

    Add login logic

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,8 @@ const router = Router();
 const bcrypt = require("bcrypt");
 const {check, validationResult} = require("express-validator");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const config = require("config")
 
 router.post(
     "/register", 
@@ -11,30 +13,61 @@ router.post(
         check("password", "Min - 6 characters").isLength({ min: 6 })
     ],
     async (req, res) => {
-    try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array(),
-                message: "Incorrect"
-             });
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: "Incorrect"
+                });
+            };
+            const {email, password} = req.body
+            const candidate = await User.findOne({ email });
+            if (candidate) {
+                return res.status(400).json({ message: "This email is already registered" });
+            };
+            hashedPassword = await bcrypt.hash(password, 12);
+            const user = new User({ email, password: hashedPassword });
+            await user.save();
+            res.status(201).json({ message: "The user is created" });
+        } catch (error) {
+            res.status(500).json( { message: "Try again :(" } );
         };
-        const {email, password} = req.body
-        const candidate = await User.findOne({ email });
-        if (candidate) {
-            return res.status(400).json({ message: "This email is already registered" });
-        };
-        hashedPassword = await bcrypt.hash(password, 12);
-        const user = new User({ email, password: hashedPassword });
-        await user.save();
-        res.status(201).json({ message: "The user is created" });
-    } catch (error) {
-        res.status(500).json( { message: "Try again :(" } );
-    };
 });
 
-router.post("/login", async (req, res) => {
-
+router.post(
+    "/login", 
+    [
+        check("email", "Incorrect email").normalizeEmail().isEmail(),
+        check("password", "Incorrect password").exists()
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: "Incorrect"
+                 });
+            }; 
+            const {email, password} = req.body;
+            const user = await User.findOne({ email });
+            if (!user) {
+              return res.status(400).json({ message: "Error :(" })  
+            };
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ message: "Password doesn't match" })
+            };
+            const token = jwt.sign(
+                { userId: user.id },
+                config.get("jwtSecret"),
+                { expiresIn: "1h" }
+            );
+            res.json({ token, userID: user.id });
+        } catch (error) {
+            res.status(500).json( { message: "Try again :(" } );
+        };
 });
 
 module.exports = router;
