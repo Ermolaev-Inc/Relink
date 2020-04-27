7f4fc3973252f391aade2125f5f43c2fd46593c2
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-04-27 22:54:06 +0500

    Add routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useRoutes } from './routes';
+import {BrowserRouter as Router} from "react-router-dom";
 
 function App() {
+  const routes = useRoutes(false);
   return (
-    <div>
-      <h1>Hello world</h1>  
-    </div>
+    <Router>
+      <div>
+        {routes}
+      </div>
+    </Router>
   );
 }
 
