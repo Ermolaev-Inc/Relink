e50b9c8b7f5b2e53a9c55c3a7e780641a9cbd728
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-05-01 21:41:13 +0500

    Add shortid logic

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -1,22 +1,38 @@
 const {Router} = reuqire("express");
 const Link = require("../models/Link");
+const config = require("config");
+const auth = require("../middleware/auth.middleware");
 const router = Router();
+const shortid = require("shortid");
 
-router.post("/generate", async (req, res) => {
+router.post("/generate", auth, async (req, res) => {
     try {
+        const baseUrl = config.get("baseUrl");
+        const {from} = req.body
+        const code = shortid.generate();
+        const existing = await Link.findOne({ from });
+        if (existing) {
+            return res.json({ link: existing });
+        };
+        const to = baseUrl + "/t/" + code;
+        const link = new Link({
+            code, to, from, owner: req.user.userId
+        });
+        await link.save();
+        res.status(201).json({ link });
     } catch (error) {
         res.status(500).json( { message: "Try again :(" } );
     };
 });
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
     try {
-        const links = await Link.find({ owner: null });
+        const links = await Link.find({ owner: req.user.userId });
         res.json(links);
     } catch (error) {
         res.status(500).json( { message: "Try again :(" } );
     };
 });
-router.get("/:id", async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
     try {
         const link = await Link.findById(req.params.id);
         res.json(link);
