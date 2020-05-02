7fffd0686acb9bec42f37045e0f90e1a1d5b260c
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-05-02 15:02:26 +0500

    Design CreatePage

--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import classes from "./CreatePage.module.css";
+import btn from "./button.svg";
 
 export const CreatePage = () => {
     return(
-        <div>
-            CreatePage
+        <div className={classes.form}>
+            <input 
+            className={classes.input_field}
+            type="text"
+            placeholder="Paste link"
+            id="link"
+            />
+            <img className={classes.btn} src={btn} alt=""/>
         </div>
     )
 };
