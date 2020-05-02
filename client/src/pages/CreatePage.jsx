a36880492ee84dc20aa1e9f5a5fe70b251dcb526
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-05-02 19:18:41 +0500

    Reducing logic is made
    In console

--- client/src/pages/CreatePage.jsx
+++ client/src/pages/CreatePage.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import classes from "./CreatePage.module.css";
 import btn from "./button.svg";
+import { useHttp } from "../hooks/http.hook"; 
+import { AuthContext } from "../context/AuthContext";
 
 export const CreatePage = () => {
+    const auth = useContext(AuthContext)
+    const {request} = useHttp();
+    const [link, setLink] = useState("");
+    const pressHandler = async () => {
+            try {
+                const data = await request("/api/link/generate", "POST", {from: link}, {
+                    Authorization: `Bearer ${auth.token }`
+                });
+                console.log(data);
+            } catch (error) {};
+    };
     return(
         <div className={classes.form}>
             <input 
@@ -10,8 +23,13 @@ export const CreatePage = () => {
             type="text"
             placeholder="Paste link"
             id="link"
+            value={link}
+            onChange={e => setLink(e.target.value)}
+            onKeyPress={pressHandler}
             />
-            <img className={classes.btn} src={btn} alt=""/>
+            <button onClick={pressHandler}>
+                <img className={classes.btn} src={btn} alt="" />
+            </button>
         </div>
     )
 };
